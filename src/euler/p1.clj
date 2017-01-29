e37065bf99f9ffec3a9b9fe138ad94de076b815c
Adam Gibbs <git@status203.ocm>
2017-01-29 21:51:19 +0000

    Starting again to get back into clj dev

---
+++ src/euler/p1.clj
@@ -0,0 +1,11 @@
+(ns euler.p1)
+
+; Find the sum of all the multiples of 3 or 5 below 1000.
+
+(def up-to 1000)
+(->> (range 1 up-to)
+     (filter #(or (= 0 (mod % 3))
+                  (= 0 (mod % 5))))
+     (apply +))
+
+; 233168
